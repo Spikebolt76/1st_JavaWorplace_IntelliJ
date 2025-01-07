75b6963ae8d947fdb52a567c82e99592c87d10cc
Spikebolt76 <168162977+Spikebolt76@users.noreply.github.com>
2025-01-07 17:08:58 +0530

    Add to git

--- Java Prog. Y.Liang/Chapter-02/Programs-case_studies/src/ShowCurrentTime.java
+++ Java Prog. Y.Liang/Chapter-02/Programs-case_studies/src/ShowCurrentTime.java
@@ -1,7 +1,36 @@
+// Greenwich Mean Time to Indian Standard Time conversion : IST = GMT + 5:30:00
+// in java, currentTimeMillis(); method from System class gives total milliseconds elapsed from 1970 1/1/1970 00:00:00 GMT
+// that's called Unix epoch time, bcs 170 was the year, Unix OS was formally introduced.
+
 public class ShowCurrentTime
 {
     public static void main(String[] args)
     {
         long totalMilliseconds = System.currentTimeMillis();
+
+        long totalSeconds = totalMilliseconds / 1000;
+        long totalMinutes = totalSeconds / 60;
+        long totalHours = totalMinutes / 60;
+
+        int currentSecond = (int) (totalSeconds % 60);
+        int currentMinute = (int) (totalMinutes % 60);
+        int currentHour = (int) (totalHours % 24);
+
+        System.out.println("Current time is => " + currentHour + ":" + currentMinute + ":" + currentSecond + " GMT");
+
+        int currentHourIST = currentHour + 5;
+        int currentMinuteIST = currentMinute + 30;
+
+        if (currentMinuteIST >= 60)
+        {
+            currentMinuteIST -= 60;
+            currentHourIST ++;
+        }
+        if (currentHourIST >= 24)
+        {
+            currentHourIST -= 24;
+        }
+
+        System.out.println("                => " + currentHourIST + ":" + currentMinuteIST + ":" + currentSecond + " IST");
     }
 }
