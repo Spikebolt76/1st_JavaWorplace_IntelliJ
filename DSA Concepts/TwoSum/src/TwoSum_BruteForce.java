a6d5cea0efaa0b7a0fdb0d2ddc8e5070c094316a
Spikebolt76 <168162977+Spikebolt76@users.noreply.github.com>
2025-01-26 20:50:46 +0530

    Add to git

--- DSA Concepts/TwoSum/src/TwoSum_BruteForce.java
+++ DSA Concepts/TwoSum/src/TwoSum_BruteForce.java
@@ -2,7 +2,20 @@ public class TwoSum_BruteForce
 {
     public static int[] twoSum(int[] arr, int target)
     {
-
+        int[] res = new int[2];
+        for (int i = 0; i < arr.length; i++)
+        {
+            for (int j = i + 1; j < arr.length; j++)
+            {
+                if (arr[i] + arr[j] == target)
+                {
+                    res[0] = i;
+                    res[1] = j;
+                    return res;
+                }
+            }
+        }
+        return res;
     }
 
     public static void main(String[] args)
